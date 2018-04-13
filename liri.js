b3b90d22abf77750ee3653c4ccf3e54425ea6149
Eric <ejzagala@gmail.com>
2018-04-13 16:02:10 -0600

    Twitter and OIMDB commands working, trouble w/ spotify creds

--- liri.js
+++ liri.js
@@ -1,10 +1,84 @@
 require("dotenv").config();
-require("./package-lock.json")
+var keys = require('./keys'); 
+var Spotify = require('node-spotify-api'); 
+var Twitter = require('twitter'); 
+var inquirer = require('inquirer'); 
+var request = require('request');
 
-var keys = require("./keys.js"); 
 
-var spotify = new Spotify(keys.spotify);
+Spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-console.log(process.argv)
+inquirer.prompt([
+{
+    name: "command", 
+    message: "What command would you like to run?", 
+    type: "rawlist",
+    choices: ["My Tweets", "Spotify this song...", "Movie this!", "Do what it says"]
+}
+]).then(function(x){
 
+    switch(x.command) {
+        case 'My Tweets':
+            var params = {screen_name: 'EJZagala'};
+            client.get('statuses/user_timeline', params, function(error, tweets, response) {
+                if (!error) {
+                    for (var i = 0; i < 20; i++) {
+                    console.log('Date tweeted: ' + tweets[i].created_at);
+                    console.log('Tweet text: ' + tweets[i].text + '\n')
+                    }
+                }
+            });
+        break; 
+        case 'Spotify this song...': 
+            inquirer.prompt([
+                {
+                    name: "songName",
+                    message: "What song would you like to look up?"
+                }
+            ]).then(function(x){
+                Spotify
+                    .search({ type: 'track', query: x.songName})
+                    .then(function(response) {
+                        console.log(response);
+                    })
+                    .catch(function(err) {
+                        console.log(err);
+                    });
+            })
+        break; 
+        case "Movie this!":
+            inquirer.prompt([
+                {
+                    name: "movieName", 
+                    message: "What movie would you like to search?"
+                }
+            ]).then(function(x){
+                var space = / /gi;
+                var nameParam = x.movieName.replace(space, '+'); 
+                var queryURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + nameParam;     
+
+                request(queryURL, function(error, response){
+                    if (error){
+                        return console.log(error); 
+                    }
+                    // console.log(response.body);
+                    movieInfo = JSON.parse(response.body); 
+                    // console.log(movieInfo);
+                    console.log(`
+                    Title: ${movieInfo.Title}
+                    Release year: ${movieInfo.Year}
+                    IMDB rating: ${movieInfo.imdbRating}
+                    Rotten Tomatoes rating: ${movieInfo.Ratings[1].Value}
+                    Country(ies) produced in: ${movieInfo.Country}
+                    Language: ${movieInfo.Language}
+                    Plot: ${movieInfo.Plot}
+                    Actors: ${movieInfo.Actors}
+                    `) 
+                })
+
+            })
+        break; 
+    }
+
+})
